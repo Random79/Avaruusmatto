373909ec7be0169a2671db6d0948aabbc0388a47
Random79 <kalle.brusi@gmail.com>
2015-02-19 21:47:54 +0200

    rotaatiot numpädin napeilla.

--- avaruusmatto/Assets/SSAurora.cs
+++ avaruusmatto/Assets/SSAurora.cs
@@ -9,6 +9,7 @@ public class SSAurora : MonoBehaviour {
 	public double myX = 0;
 	public double myY = 0;
 	public double myZ = 0;
+  
 
 	// aluksen massa (kg) 
 	// työntövoima (N) tämä on konventionaalinen työntövoima esim. dokkausta, väistöliikkeitä ja aluksen valtausta varten. 
@@ -21,11 +22,12 @@ public class SSAurora : MonoBehaviour {
 	public float myVelZ = 0f;
 	public float myVelocity = 0f;
 
+	public float maxAngularVel = 10f;
 
 	//v = myThrust/myMass *Delta.time 
 
 	// vector3:n tallennettu pyörimisnopeus (localAngularVelocity.x, localAngularVelocity.y, localAngularVelocity.z)
-	//Vector3 myAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
+	// Vector3 myAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
 
 	// Use this for initialization
 	void Start () {
@@ -42,21 +44,63 @@ public class SSAurora : MonoBehaviour {
 		var rotY = transform.localRotation.y;
 		var rotZ = transform.localRotation.z;
 
-		if (Input.GetKey (KeyCode.Keypad8)) {
+			
+		// vector3:n tallennettu pyörimisnopeus (localAngularVelocity.x, localAngularVelocity.y, localAngularVelocity.z)
+		Vector3 localAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
+
+		if (Input.GetKey (KeyCode.R)) {
 			myVelX = myVelX + myThrust/rigidbody.mass*Time.fixedDeltaTime;	
+			//rigidbody.AddRelativeForce (0, 0.1f, 0);
 		}
-		else if (Input.GetKey (KeyCode.Keypad0)){
+		if (Input.GetKey (KeyCode.Q)){
 			myVelX = 0;
 		}
+
 		myX += myVelX * Time.fixedDeltaTime;
 
-		var param = new double[3];
+		var param = new double[3];  //TODO tän vois nimetä myPositionVectoriksi. lisäks tähän vois laittaa loppuun rotaatiot. 
 		param [0] = myX;
 		param [1] = myY;
 		param [2] = myZ;
 
+		if (Input.GetKey (KeyCode.Keypad8)) {
+			rigidbody.AddRelativeTorque (0, 0, -100000);
+		}
 		if (Input.GetKey (KeyCode.Keypad2)) {
-			rigidbody.AddRelativeTorque (0, 0, 10000); 
+			rigidbody.AddRelativeTorque (0, 0, 100000); 
+		}
+		if (Input.GetKey (KeyCode.Keypad4)) {
+			rigidbody.AddRelativeTorque (0, -100000, 0); 
+		}
+		if (Input.GetKey (KeyCode.Keypad6)) {
+			rigidbody.AddRelativeTorque (0, 100000, 0); 
+		}
+		if (Input.GetKey (KeyCode.Keypad7)) {
+			rigidbody.AddRelativeTorque (100000, 0, 0); 
+		}
+		if (Input.GetKey (KeyCode.Keypad9)) {
+			rigidbody.AddRelativeTorque (-100000, 0, 0); 
+		}
+		if (Input.GetKey (KeyCode.Keypad5)) {
+
+			if (localAngularVelocity.x < 0) {
+				rigidbody.AddRelativeTorque (100000, 0, 0); 
+			}
+			if (localAngularVelocity.x > 0) {
+				rigidbody.AddRelativeTorque (-100000, 0, 0); 
+			}
+			if (localAngularVelocity.y < 0) {
+				rigidbody.AddRelativeTorque (0, 100000, 0); 
+			}
+			if (localAngularVelocity.y > 0) {
+				rigidbody.AddRelativeTorque (0, -100000, 0); 
+			}
+			if (localAngularVelocity.z < 0) {
+				rigidbody.AddRelativeTorque (0, 0, 100000); 
+			}
+			if (localAngularVelocity.z > 0) {
+				rigidbody.AddRelativeTorque (0, 0, -100000); 
+			}
 		}
 
 
