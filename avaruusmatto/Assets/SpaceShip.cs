d767784512aa747e6fd02ca3b49039870811ec2f
mcine <mcine74@hotmail.com>
2015-02-26 11:13:30 +0200

    alotettu waypont tukea

--- avaruusmatto/Assets/SpaceShip.cs
+++ avaruusmatto/Assets/SpaceShip.cs
@@ -1,8 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class Waypoint {
+	public Waypoint(double x, double y, double z)
+	{
+		X=x;
+		Y=y;
+		Z=z;
+	}
+	public double X;
+	public double Y;
+	public double Z;
+}
 
 public class SpaceShip : SpaceObject {
 
+	public List<Waypoint> Waypoints = new List<Waypoint>();
+
+	int currentWaypoint = 0;
 	public float myThrust = 1000000;
 	public float myVelX = 10f;
 	public float myVelY = 0f;
@@ -14,11 +30,22 @@ public class SpaceShip : SpaceObject {
 
 	// Use this for initialization
 	void Start () {
-		myX = -10;
-		myVelX = 1;
+
+		Waypoints.Add(new Waypoint(0,1,5));
+		if(Waypoints.Count>0)
+			SetDestination(Waypoints[0]);
 	}
 	
 	void FixedUpdate() {
+
+		//TODO: if at waypoint, SetDestination
+		if(isAtWaypoint(Waypoints[currentWaypoint]))
+		{
+			currentWaypoint++;
+			if(Waypoints.Count>currentWaypoint)
+				SetDestination(Waypoints[currentWaypoint]);
+			else Stop();
+		}
 		myVelocity = Mathf.Sqrt (Mathf.Pow (myVelX,2) + Mathf.Pow (myVelY,2) + Mathf.Pow (myVelZ,2));
 
 		Vector3 localAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
@@ -28,5 +55,24 @@ public class SpaceShip : SpaceObject {
 		myZ += myVelZ*Time.fixedDeltaTime;
 
 		UpdatePosition();
+	}
+
+	bool isAtWaypoint(Waypoint w)
+	{
+		double distance = Mathf.Sqrt (Mathf.Pow (w.X-myX,2) + Mathf.Pow (w.Y-myY,2) + Mathf.Pow (w.Z-myZ,2));
+		if(distance<0) distance = 0-distance;
+		if(distance < 2) return true;
+		return false;
+	}
+
+	void SetDestination(Waypoint w)
+	{
+		myVelX = 1;
+		// TODO; tee jotain järkevää
+	}
+
+	void Stop()
+	{
+		myVelX =  myVelY = myVelZ = 0;
 	}
 }
