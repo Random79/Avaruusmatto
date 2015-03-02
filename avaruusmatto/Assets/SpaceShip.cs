8a1bfa802e0a575b26c9dabe725e7060989cf15b
Random79 <kalle.brusi@gmail.com>
2015-03-02 22:15:28 +0200

    käännöstä v003
    ei toimi vielä, mutta peruslogiikka paikoillaan.

--- avaruusmatto/Assets/SpaceShip.cs
+++ avaruusmatto/Assets/SpaceShip.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Waypoint {
 	public Waypoint(double x, double y, double z)
 	{
@@ -40,8 +40,14 @@ public class SpaceShip : SpaceObject {
 	
 	public float maxAngularVel = 10f;
 	bool IsTurning = false;
+	public byte turningState = 0; 
+	public bool turnXToPos = true;
+	public bool turnYToPos = true;
+
 	Waypoint deltaVector;
 	public Quaternion deltaRotation;
+
+	public Vector3 localAngularVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -55,6 +61,9 @@ public class SpaceShip : SpaceObject {
 	
 	void FixedUpdate() {
 
+		// vector3:n tallennettu pyörimisnopeus (localAngularVelocity.x, localAngularVelocity.y, localAngularVelocity.z)
+		localAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
+
 		//TODO: if at waypoint, SetDestination
 		if(isAtWaypoint(Waypoints[currentWaypoint]))
 		{
@@ -65,12 +74,7 @@ public class SpaceShip : SpaceObject {
 			{
 				IsTurning=true;
 				var currentWp=Waypoints[currentWaypoint];
-				deltaVector = Waypoints[currentWaypoint] - new Waypoint(myX,myY,myZ);
-				var magnitude = System.Math.Sqrt (System.Math.Pow (deltaVector.X,2) + System.Math.Pow (deltaVector.Y,2) + System.Math.Pow (deltaVector.Z,2));
-				deltaVector = deltaVector/magnitude;
 
-				deltaRotation = Quaternion.FromToRotation(new Vector3 ((float)deltaVector.X,(float)deltaVector.Y,(float)deltaVector.Z), 
-				                                          new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z));
 				// quaternioni euleriksi
  				
 				// tarkista onko deltarotation.x negatiivinen vai positiivinen
@@ -86,8 +90,7 @@ public class SpaceShip : SpaceObject {
 
 
 		myVelocity = Mathf.Sqrt (Mathf.Pow (myVelX,2) + Mathf.Pow (myVelY,2) + Mathf.Pow (myVelZ,2));
-
-		Vector3 localAngularVelocity = rigidbody.transform.InverseTransformDirection(rigidbody.angularVelocity);
+		
 
 		myX += myVelX*Time.fixedDeltaTime;
 		myY += myVelY*Time.fixedDeltaTime;
@@ -103,41 +106,138 @@ public class SpaceShip : SpaceObject {
 		if(distance < 2) return true;
 		return false;
 	}
-
+	//TODO pitääkö tällanen voidi olla fixedupdatessa, vai toimiiko ilman?
 	void SetDestination(Waypoint w)
 	{
-	
-		if( Mathf.Abs( deltaRotation.eulerAngles.x - transform.forward.x ) < 0.001 && Mathf.Abs( deltaRotation.eulerAngles.y - transform.forward.y) < 0.001)
+		//pysäytetään rotaatio
+		if(IsTurning==true && turningState==0 && localAngularVelocity.magnitude > 0.01) 
 		{
-			IsTurning=false;
-			return;
+			stopRotation();
 		}
-		if (deltaRotation.eulerAngles.x < 0){
-			if (transform.forward.x < deltaRotation.eulerAngles.x/2){
-				rigidbody.AddRelativeTorque (100000, 0, 0);
-			}
-			if (transform.forward.x > deltaRotation.eulerAngles.x/2){
-				rigidbody.AddRelativeTorque (-100000, 0, 0);
-			}
+		//kun rotaatio on pysähtynyt, niin mennään seuraavaan tilaan
+		if(IsTurning==true && turningState==0 && localAngularVelocity.magnitude >= 0.01) 
+		{
+			turningState=1;
+		}
+		// tilassa 1 alustetaan parametrit kääntymmistä varten
+		if(turningState==1)
+		{
+			//vektori omasta pisteestä waypointille
+			deltaVector = Waypoints[currentWaypoint] - new Waypoint(myX,myY,myZ);
+			// yksikkövektori. eli komponentien neliösumman neliöjuuri on 1. 
+			var magnitude = System.Math.Sqrt (System.Math.Pow (deltaVector.X,2) + 
+							System.Math.Pow (deltaVector.Y,2) + System.Math.Pow (deltaVector.Z,2));
+			// tämä on yhden pituinen vektori, joka osoittaa waypointin suuntaan.
+			deltaVector = deltaVector/magnitude;
+			// tämä on quaternioni muodossa oleva pyörähdysjuttu waypointin suuntaan
+			deltaRotation = Quaternion.FromToRotation(new Vector3 ((float)deltaVector.X,(float)deltaVector.Y,(float)deltaVector.Z), 
+			                                          new Vector3(transform.rotation.x,transform.rotation.y,transform.rotation.z));
+
+			// tarkistetaan, kumpaan suuntaan pitää kääntyä x ja y akselien suhteen
+			if (deltaRotation.eulerAngles.x > 0) turnXToPos = false; 
+			if (deltaRotation.eulerAngles.y > 0) turnYToPos = false; 
+			turningState=2;
+		}
+		// tila 2 tekee käännökset
+		if(turningState==2)
+		{
+			if (turnXToPos){
+				if (transform.forward.x < deltaRotation.eulerAngles.x/2){
+					rigidbody.AddRelativeTorque (100000, 0, 0);
+				}
+				if (transform.forward.x > deltaRotation.eulerAngles.x/2){
+					rigidbody.AddRelativeTorque (-100000, 0, 0);
+				}
+				
+			} 
 			
-		} 
-		
-		if (deltaRotation.eulerAngles.x > 0){
-			if (transform.forward.x > deltaRotation.eulerAngles.x/2){
-				rigidbody.AddRelativeTorque (-100000, 0, 0);
-			}
-			if (transform.forward.x > deltaRotation.eulerAngles.x/2){
-				rigidbody.AddRelativeTorque (100000, 0, 0);
-			}
+			if (turnXToPos==false){
+				if (transform.forward.x > deltaRotation.eulerAngles.x/2){
+					rigidbody.AddRelativeTorque (-100000, 0, 0);
+				}
+				if (transform.forward.x > deltaRotation.eulerAngles.x/2){
+					rigidbody.AddRelativeTorque (100000, 0, 0);
+				}
+				
+			} 
+			if (turnYToPos){
+				if (transform.forward.y < deltaRotation.eulerAngles.y/2){
+					rigidbody.AddRelativeTorque (0, 100000, 0);
+				}
+				if (transform.forward.y > deltaRotation.eulerAngles.y/2){
+					rigidbody.AddRelativeTorque (0, -100000, 0);
+				}
+				
+			} 
 			
-		} 
+			if (turnYToPos==false){
+				if (transform.forward.y > deltaRotation.eulerAngles.y/2){
+					rigidbody.AddRelativeTorque (0, -100000, 0);
+				}
+				if (transform.forward.y > deltaRotation.eulerAngles.y/2){
+					rigidbody.AddRelativeTorque (0, 100000, 0);
+				}
+				
+			}
+			// sit, kun ollaan päästy oikeaan suuntaan, vaihdetaan tila
+			if( Mathf.Abs( deltaRotation.eulerAngles.x - transform.forward.x ) < 0.001 && Mathf.Abs( deltaRotation.eulerAngles.y - transform.forward.y) < 0.001)
+			{
+				turningState=3;
+			}
+		}
+		// pysäytetään rotaatio vielä varmuuden vuoksi
+		if (turningState == 3 && localAngularVelocity.magnitude >= 0.01)
+		{
+			stopRotation();
+		}
+		if (turningState == 3 && localAngularVelocity.magnitude < 0.01)
+		{
+			turningState=4;
 
-
-
+		}
+		// tarkistetaan vielä, onko suunta oikea
+		if (turningState == 4)
+		{
+			// jos ei ole oikea, niin mennään uudestaan tilaan 0 ja aloitetaan alusta
+			if( Mathf.Abs( deltaRotation.eulerAngles.x - transform.forward.x ) > 0.001 && Mathf.Abs( deltaRotation.eulerAngles.y - transform.forward.y) > 0.001)
+			{
+				turningState=0;
+			}
+			// muuten lopetetaan kääntyminen
+			else 
+			{
+				IsTurning=false;
+				turningState=0;
+			}
+		}
+		return;
 	}
-
+	
 	void Stop()
 	{
 		myVelX =  myVelY = myVelZ = 0;
 	}
+
+	void stopRotation()
+	{
+		if (localAngularVelocity.x < 0) {
+			rigidbody.AddRelativeTorque (100000, 0, 0); 
+		}
+		if (localAngularVelocity.x > 0) {
+			rigidbody.AddRelativeTorque (-100000, 0, 0); 
+		}
+		if (localAngularVelocity.y < 0) {
+			rigidbody.AddRelativeTorque (0, 100000, 0); 
+		}
+		if (localAngularVelocity.y > 0) {
+			rigidbody.AddRelativeTorque (0, -100000, 0); 
+		}
+		if (localAngularVelocity.z < 0) {
+			rigidbody.AddRelativeTorque (0, 0, 100000); 
+		}
+		if (localAngularVelocity.z > 0) {
+			rigidbody.AddRelativeTorque (0, 0, -100000); 
+		}
+	}
+
 }
